fda4e7dd9094b70b40befd3429c95bd45f790cb7
Rajiv <kittdotech@gmail.com>
2016-02-16 10:46:10 +0530

    Added CSV in Admin

--- inference2/admin.py
+++ inference2/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from inference2.models import Define3
+from django.contrib.auth.models import User
 
-# Register your models here.
+from django.contrib import admin
+from inference2.models import Define3
+from django.contrib import admin
+
+from admin_exporter.actions import export_as_csv_action
+from admin_exporter.actions import export_as_json_action
+from admin_exporter.actions import export_as_xml_action
+from admin_exporter.actions import export_as_yaml_action
+
+
+admin.site.add_action(export_as_csv_action)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('type','word', 'rel','definition')
+
+admin.site.register(Define3, AuthorAdmin)
