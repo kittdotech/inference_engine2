abaed3fe28e2994c21d9e76e786a8b2a25f27efc
Rajiv <kittdotech@gmail.com>
2016-02-18 10:53:52 +0530

    Fixed Empty definition

--- inference2/actions.py
+++ inference2/actions.py
@@ -40,74 +40,78 @@ def changesymbol(queryset,mode):
 	#data = json.loads(data)
 	#for item in data:
 	#	print
+
 	if mode=='TtoS':
 		for x in queryset:
 			original_text = x.definition
-			if '>>' in original_text:
-				original_text=original_text.replace('>>',unichr(8835))
-			if 'ta^' in original_text:
-				original_text=original_text.replace('ta^',unichr(8868))
-			if 'co^' in original_text:
-				original_text=original_text.replace('co^',unichr(8869))
-			if ';' in original_text:
-				original_text=original_text.replace(';',unichr(172))
-			if '<>' in original_text:
-				original_text=original_text.replace('<>',unichr(8801))
-			if ' c^' in original_text:
-				original_text=original_text.replace('c^',unichr(8658))
-			if '#' in original_text:
-				original_text=original_text.replace('#',unichr(8703))
-			if 'i^' in original_text:
-				original_text=original_text.replace('i^',unichr(8866))
-			if '>' in original_text:
-				original_text=original_text.replace('>',unichr(8594))
-			if 'nf^' in original_text:
-				original_text=original_text.replace('nf^',unichr(8876))
-			if 'ed^' in original_text:
-				original_text=original_text.replace('ed^',unichr(8891))
-			if '+' in original_text:
-				original_text=original_text.replace('+',unichr(8744))
-			if '&&' in original_text:
-				original_text=original_text.replace('&&',unichr(8896))
-			if '@' in original_text:
-				original_text=original_text.replace('@',unichr(8855))
-			if 'if^' in original_text:
-				original_text=original_text.replace('if^',unichr(8660))
+			if original_text:
+
+				if '>>' in original_text:
+					original_text=original_text.replace('>>',unichr(8835))
+				if 'ta^' in original_text:
+					original_text=original_text.replace('ta^',unichr(8868))
+				if 'co^' in original_text:
+					original_text=original_text.replace('co^',unichr(8869))
+				if ';' in original_text:
+					original_text=original_text.replace(';',unichr(172))
+				if '<>' in original_text:
+					original_text=original_text.replace('<>',unichr(8801))
+				if ' c^' in original_text:
+					original_text=original_text.replace('c^',unichr(8658))
+				if '#' in original_text:
+					original_text=original_text.replace('#',unichr(8703))
+				if 'i^' in original_text:
+					original_text=original_text.replace('i^',unichr(8866))
+				if '>' in original_text:
+					original_text=original_text.replace('>',unichr(8594))
+				if 'nf^' in original_text:
+					original_text=original_text.replace('nf^',unichr(8876))
+				if 'ed^' in original_text:
+					original_text=original_text.replace('ed^',unichr(8891))
+				if '+' in original_text:
+					original_text=original_text.replace('+',unichr(8744))
+				if '&&' in original_text:
+					original_text=original_text.replace('&&',unichr(8896))
+				if '@' in original_text:
+					original_text=original_text.replace('@',unichr(8855))
+				if 'if^' in original_text:
+					original_text=original_text.replace('if^',unichr(8660))
 			x.definition = original_text
 			x.save()
 	if mode=='StoT':
 		for x in queryset:
 			original_text = x.definition
-			if  unichr(8835)in original_text:
-				original_text=original_text.replace(unichr(8835),'>>')
-			if  unichr(8868)in original_text:
-				original_text=original_text.replace(unichr(8868),'ta^')
-			if  unichr(8869)in original_text:
-				original_text=original_text.replace(unichr(8869),'co^' )
-			if  unichr(172)in original_text:
-				original_text=original_text.replace(unichr(172),';')
-			if  unichr(8801)in original_text:
-				original_text=original_text.replace(unichr(8801),'<>' )
-			if  unichr(8658)in original_text:
-				original_text=original_text.replace(unichr(8658),' c^')
-			if  unichr(8703)in original_text:
-				original_text=original_text.replace(unichr(8703),'#')
-			if  unichr(8866)in original_text:
-				original_text=original_text.replace(unichr(8866),'i^')
-			if unichr(8594)in original_text:
-				original_text=original_text.replace(unichr(8594),'>' )
-			if  unichr(8876)in original_text:
-				original_text=original_text.replace(unichr(8876),'nf^')
-			if  unichr(8891)in original_text:
-				original_text=original_text.replace(unichr(8891),'ed^')
-			if  unichr(8744)in original_text:
-				original_text=original_text.replace(unichr(8744),'+')
-			if  unichr(8896)in original_text:
-				original_text=original_text.replace(unichr(8896),'&&')
-			if  unichr(8855)in original_text:
-				original_text=original_text.replace(unichr(8855),'@')
-			if  unichr(8660)in original_text:
-				original_text=original_text.replace(unichr(8660),'if^')
+			if original_text:
+				if  unichr(8835)in original_text:
+					original_text=original_text.replace(unichr(8835),'>>')
+				if  unichr(8868)in original_text:
+					original_text=original_text.replace(unichr(8868),'ta^')
+				if  unichr(8869)in original_text:
+					original_text=original_text.replace(unichr(8869),'co^' )
+				if  unichr(172)in original_text:
+					original_text=original_text.replace(unichr(172),';')
+				if  unichr(8801)in original_text:
+					original_text=original_text.replace(unichr(8801),'<>' )
+				if  unichr(8658)in original_text:
+					original_text=original_text.replace(unichr(8658),' c^')
+				if  unichr(8703)in original_text:
+					original_text=original_text.replace(unichr(8703),'#')
+				if  unichr(8866)in original_text:
+					original_text=original_text.replace(unichr(8866),'i^')
+				if unichr(8594)in original_text:
+					original_text=original_text.replace(unichr(8594),'>' )
+				if  unichr(8876)in original_text:
+					original_text=original_text.replace(unichr(8876),'nf^')
+				if  unichr(8891)in original_text:
+					original_text=original_text.replace(unichr(8891),'ed^')
+				if  unichr(8744)in original_text:
+					original_text=original_text.replace(unichr(8744),'+')
+				if  unichr(8896)in original_text:
+					original_text=original_text.replace(unichr(8896),'&&')
+				if  unichr(8855)in original_text:
+					original_text=original_text.replace(unichr(8855),'@')
+				if  unichr(8660)in original_text:
+					original_text=original_text.replace(unichr(8660),'if^')
 			x.definition = original_text
 			x.save()
 
