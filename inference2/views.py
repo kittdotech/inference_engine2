2f6f068b0e20d3f1302e41181d3e73b3f42b4b7f
Rajiv <kittdotech@gmail.com>
2016-02-26 14:14:16 +0530

    Added Guide Proof

--- inference2/views.py
+++ inference2/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.shortcuts import render
 from inference2.models import Define3
 from django.core.serializers.json import DjangoJSONEncoder
-
+from . import guided_proof
 import json
 from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
+
+
 def index(request):
-    rows=Define3.objects.all()
+    result={}
+    if request.method=='POST':
+        post_data=request.POST.copy()
+        post_data = guided_proof.get_result(request.POST.copy())
+        result=json.dumps(post_data,cls=DjangoJSONEncoder)
+
     #rows = json.dumps(rows,cls=DjangoJSONEncoder)
-    return render(request,"inference2/index.html",{'rows':rows})
+    return render(request,"inference2/index.html",{'result':result})
