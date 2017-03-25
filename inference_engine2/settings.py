ffa5e667f7f61c34333924dc0e44164c614bc259
agam <duahimanshu100@gmail.com>
2017-03-25 17:17:54 +0530

    heroku n aws DB

--- inference_engine2/settings.py
+++ inference_engine2/settings.py
@@ -91,12 +91,13 @@ CONFIG_TYPE = 1  # os.getenv('CLEARDB_DATABASE_URL','')
 if CONFIG_TYPE:
     DICT = {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'inference_engine',
-        'HOST': '34.208.135.45',
-        'USER': 'root',
-        'PASSWORD': 'password',
+        'NAME': 'heroku_e080b104b4741bc',
+        'HOST': 'us-cdbr-iron-east-03.cleardb.net',
+        'USER': 'bddaf22a8f080b',
+        'PASSWORD': 'c4c504bd',
 
     }
+
 if not CONFIG_TYPE:
     DEVELOP = os.getenv('DEVELOP', '')
     DB_PASSWORD = 'deductive'
@@ -105,9 +106,10 @@ if not CONFIG_TYPE:
     DICT = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'inference_engine',
-        'HOST': 'localhost',
+        'HOST': '34.208.135.45',
         'USER': 'root',
-        'PASSWORD': DB_PASSWORD,
+        'PASSWORD': 'password',
+
     }
 
 DATABASES = {
