c6741558662a4e1d94ce0f248403528208c19fc3
agam <duahimanshu100@gmail.com>
2017-05-24 20:42:36 +0530

    Add Name in Instruction File

--- inference2/models.py
+++ inference2/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 
 class InstructionFile(models.Model):
+    name = models.CharField(
+        max_length=100, default='name')
     data = models.FileField(upload_to='./static/inference2/')
     FILE_TYPE_CHOICES = (
         ('0', 'instruction'),
